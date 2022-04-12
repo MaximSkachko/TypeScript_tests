0367b85f80371f2452288a739c5c85db346fd188
Maxim <maxim.skachko1997@gmail.com>
2022-04-12 15:58:37 +0300

    test5 updated

--- test/specs/test5_datepicker.ts
+++ test/specs/test5_datepicker.ts
@@ -16,11 +16,8 @@ const day = new Date()
 const month = day.getMonth()
 const year = day.getFullYear()
 const daysInMonth = new Date(year, month, 30).getDate(); // Не совсем понятно как это работает
-const addDays = new Date().getDate() + 3;
+const addDays = new Date().getDate() + 3
 let newMonth = new Date(new Date().setMonth(new Date().getMonth() + 1)).toLocaleString('en', { month: 'long' })
-
-
-
 
 describe ('datepicker', () => {
     it ('how to use datepicker', async () => {
@@ -30,18 +27,18 @@ describe ('datepicker', () => {
         await browser.switchToFrame(await browser.$('.demo-frame.lazyloaded'))
         await (await browser.$('#datepicker')).waitForDisplayed()
         await (await browser.$('#datepicker')).click()
-        await browser.pause(1000)
+        //await browser.pause(1000)
         await (await browser.$('.ui-datepicker-next.ui-corner-all')).click()
         if (daysInMonth <= addDays) {
             await (await browser.$('.ui-datepicker-next.ui-corner-all')).click()
             newMonth = new Date(new Date().setMonth(new Date().getMonth() + 2)).toLocaleString('en', { month: 'long' })
-            await browser.pause(1000)
+            //await browser.pause(1000)
         }
         expect(await (await browser.$('.ui-datepicker-month')).getText()).to.equal(newMonth)
         await (await browser.$(`//a[@class="ui-state-default" and .='${addDays}']`)).click()
-        await browser.pause(10000)
-        // добавить expect на проверку инпута даты. Удалить лишние паузы. Заменить на waitForDisplayed
-        // to.equal(May) - переменная
+        //await browser.pause(1000)
+        expect(await (await browser.$('.hasDatepicker')).getValue()).to.equal(`05/${addDays}/${year}`)
     })
 })
-// получить текущую дату js (одна константа) + 1 месяц и три дня(вторая константа). Создать if на проверку кол-ва дней в текущем месяце <= текущему дню + 3 дня. Если больше, то двойной клик по месяцу
+// Получить все li (https://www.globalsqa.com/demo-site/auto-complete/#Categories). Написать цикл по массиву. Если текст равен 'annk K12', то кликнуть по нему и выйти из цикла (return/досрочно выйти из цикла если сработал if)
+// https://www.globalsqa.com/demo-site/auto-complete/#ComboBox клинуть кнопку. Пролистать до последнего элемента и выбрать его. Во втором начать вводить javas, Выбрать JS
