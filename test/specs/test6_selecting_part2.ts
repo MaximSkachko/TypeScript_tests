643ae8ae33c6241b9c16bf994b69e362f5aa3533
Maxim <maxim.skachko1997@gmail.com>
2022-05-02 16:06:51 +0300

    changed the class

--- test/specs/test6_selecting_part2.ts
+++ test/specs/test6_selecting_part2.ts
@@ -11,11 +11,10 @@ describe ('Selection', () => {
         await (await browser.$('option*=Scheme')).scrollIntoView()
         await (await browser.$('option*=Scheme')).click()
         expect(await (await browser.$('#combobox')).getValue()).to.equal('Scheme')
-        await (await browser.$('.custom-combobox-input.ui-widget.ui-widget-content.ui-state-default.ui-corner-left.ui-autocomplete-input')).setValue('javas')
+        await (await browser.$('.custom-combobox-input.ui-widget.ui-widget-content')).setValue('javas')
         await (await browser.$('#ui-id-1')).click()
-        expect(await (await browser.$('.custom-combobox-input.ui-widget.ui-widget-content.ui-state-default.ui-corner-left.ui-autocomplete-input')).getValue()).to.equal('JavaScript')
+        expect(await (await browser.$('.custom-combobox-input.ui-widget.ui-widget-content')).getValue()).to.equal('JavaScript')
         expect(await (await browser.$('#combobox')).getValue()).to.equal('JavaScript')
     })
 })     
-// Получить все li (https://www.globalsqa.com/demo-site/auto-complete/#Categories). Написать цикл по массиву. Если текст равен 'annk K12', то кликнуть по нему и выйти из цикла (return/досрочно выйти из цикла если сработал if)
 // https://www.globalsqa.com/demo-site/auto-complete/#ComboBox клинуть кнопку. Пролистать до последнего элемента и выбрать его. Во втором начать вводить javas, Выбрать JS
